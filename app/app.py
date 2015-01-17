1564459c14360be13cdf32fcd370beb45078d456
junojunho <wnsgh611@gmail.com>
2015-01-17 15:32:40 +0900

    로그인 기초

--- app/app.py
+++ app/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -22,5 +26,16 @@ def index():
                            title='Flask',
                            posts=posts,
                            user=user)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/login_process', methods=['POST'])
+def login_process():
+    if request.method == 'POST':
+        return request.form['user_email']
 
 app.run(debug=True)
