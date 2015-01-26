ca4526f954856d90f4f544ae35255f5ce52d11db
junojunho <wnsgh611@gmail.com>
2015-01-26 17:20:52 +0900

    관리자 페이지 추가를 위한 테이블 칼럼 수정 패키지 (마이그레이션) 추가 및 init 관련 코드 추가.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask.ext.bcrypt import Bcrypt
+from flask_migrate import Manager
+from flask_migrate import Migrate
+from flask_migrate import MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object('config')
-bcrypt = Bcrypt(app)
 
+app = Flask(__name__)
+# Importing configuration from config file
+app.config.from_object('config')
+# For Password Scramble
+bcrypt = Bcrypt(app)
+# Create Manager instance for using Database Migration
+manager = Manager(app)
+# Database using SQLAlchemy
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
 
 from controllers import *
